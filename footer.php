c028e52b732173472190df7058ec512f42058032
bubliksite <serj.solntsev@gmail.com>
2021-04-12 15:56:26 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+        <footer class="bg-dark-blue py-5 text-white">
+            <div class="container">
+                <div class="mx-lg-5 px-lg-5 d-flex justify-content-between">
+                    <div class="logos d-flex justify-content-center">
+                        <img src="images/Logo_1.svg" alt="">
+                        <img src="images/Logo_2.svg" alt="">
+                    </div>
+                    <div class="captions">
+                        <p class="lead">Contact Us</p>
+                        <p>
+                            ROND YACHTING LLC (Russia) Troitsky,6 lit A, <br>
+                            190005, Saint-Petrsburg, Russia.
+                        </p>
+                        <a href="tel:+7(921)857-81-81" class="text-white text-decoration-none">
+                            +7 (921) 857-81-81
+                        </a>
+                        <br>
+                        <a href="mailto:elling.yachts@gmail.com" class="text-white text-decoration-none">
+                            elling.yachts@gmail.com
+                        </a>
+                    </div>
+                </div>
+                <hr class="my-5">
+                <div class="mx-lg-5 px-lg-5 d-flex justify-content-between">
+                    <small class="text-muted">
+                        © 2021  Elling yachts . All rights reserved
+                    </small>
+                    <div class="social-images-group">
+                        <img src="images/Instagram.svg" alt="">
+                        <img src="images/Dribbble.svg" alt="">
+                        <img src="images/Twitter.svg" alt="">
+                        <img src="images/Youtube.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    </body>
+</html>
